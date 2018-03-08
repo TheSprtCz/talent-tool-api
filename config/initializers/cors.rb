cd03dda0e6db91a79f53dc1d8fa4c77ee49f9b6b
TheSprtCz <halapetr@selfnet.cz>
2018-03-08 13:23:51 +0100

    Convert to PGSQL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ENV["RB_CORS"] || 'http://localhost:3000'
 
     resource '*',
       headers: :any,
